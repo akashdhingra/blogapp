e0171a7b97521ed8ea4b1e122b72d8bf3c4d7127
Akash Dhingra <akashdhingra1996@gmail.com>
2024-03-06 13:39:08 -0500

    building home page

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from django.urls import  path
 from app import views
 
 urlpatterns = [
+    path('',views.index,name='index'),
     path('post/<slug:slug>',views.post_page,name='post_page')
+    
 ]
